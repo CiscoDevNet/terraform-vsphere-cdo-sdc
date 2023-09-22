cf63ebd6c2c146c7b9d654a8048055c5f9d59354
Siddhu Warrier <siwarrie@cisco.com>
2023-09-22 16:32:46 +0100

    chore(amazon-linux): use FIPS compliant Amazon Linux 2 images for SDC

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
-  us_ovf_url  = "https://s3.us-east-2.amazonaws.com/dl.sdc.us-east-2.labs.cdo.cisco.com/CDO-SDC-AUTOPROVISIONED-VM-DEVELOPMENT.ova"
-  eu_ovf_url  = "https://s3.eu-central-1.amazonaws.com/dl.sdc.eu-central-1.labs.cdo.cisco.com/CDO-SDC-AUTOPROVISIONED-VM-DEVELOPMENT.ova"
+  us_ovf_url  = "https://s3.us-east-2.amazonaws.com/dl.sdc.us-east-2.labs.cdo.cisco.com/CDO-SDC-AUTOPROVISIONED-VM-50105b2.ova"
+  eu_ovf_url  = "https://s3.eu-central-1.amazonaws.com/dl.sdc.eu-central-1.labs.cdo.cisco.com/CDO-SDC-AUTOPROVISIONED-VM-50105b2.ova"
   folder_path = "cdo-tenant-${var.cdo_tenant_name}"
   memory_mb   = 4096
   disk_gb     = 64
